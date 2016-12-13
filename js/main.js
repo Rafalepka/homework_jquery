5ea857b1356f59766866c42e93fd5720f0ca7d83
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-13 13:31:38 +0100

    last version
    dodanie komentarzy

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 $(function(){
    
-    $("div").css({"background-color": "red", "height": "200px", "width": "200px"});
+    $("div").css({"background-color": "red", "height": "200px", "width": "200px"}); //nadanie właściwości div
     
-    $("button").click(function(){
+    $("button").click(function(){ //funkcja click button
         
-        $("div").animate({"margin-left": "100px", "height": "100px", "width": "100px"}, 3000, niebieski);
+        $("div").animate({"margin-left": "100px", "height": "100px", "width": "100px"}, 3000, niebieski); // animacja po kliknieciu w przycisk 
         
-        function niebieski() {
+        function niebieski() { //funkcja zmiany koloru przycisku po click
             
-           $("div").animate({backgroundColor: "blue"}, 5000, napis);
+           $("div").animate({backgroundColor: "blue"}, 5000, napis); // zmiana koloru i wywolanie napisu
 			
 			function napis() {
-				$("div").add("h2").text("Gratulacje, animacja zakończona!").css({"color": "white"});
+                
+				$("div").add("h2").text("Gratulacje, animacja zakończona!").css({"color": "white"}); // nadanie wlasciwosci div ze pojawia się w nim napis oraz jego kolor 
 			}
         }
         
